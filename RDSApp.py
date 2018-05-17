b9b19a5299c1eb92c4d95d075710a9838c3db1ec
ubaldoatencia <ubaldo.atencia@careanalytics.com>
2018-05-17 13:08:14 -0500

    Error handler

--- RDSApp.py
+++ RDSApp.py
@@ -2,43 +2,37 @@ import pandas as pd
 from sqlalchemy import create_engine
 import configparser
 
-config = configparser.ConfigParser()  
-config.read('RDSConfig.ini')
-src_db_endpoint = config['SOURCE']['DB_ENDPOINT']
-src_db_port     = config['SOURCE']['DB_PORT']
-src_db_name     = config['SOURCE']['DB_NAME']
-src_db_usr      = config['SOURCE']['DB_USR']
-src_db_pwd      = config['SOURCE']['DB_PWD']
+try:
+    step_msg        = "Reading config file."
+    config = configparser.ConfigParser()
+    config.read('RDSConfig.ini')
 
-trg_db_endpoint = config['TARGET']['DB_ENDPOINT']
-trg_db_port     = config['TARGET']['DB_PORT']
-trg_db_name     = config['TARGET']['DB_NAME']
-trg_db_usr      = config['TARGET']['DB_USR']
-trg_db_pwd      = config['TARGET']['DB_PWD']
+    src_db_endpoint = config['SOURCE']['DB_ENDPOINT']
+    src_db_port     = config['SOURCE']['DB_PORT']
+    src_db_name     = config['SOURCE']['DB_NAME']
+    src_db_usr      = config['SOURCE']['DB_USR']
+    src_db_pwd      = config['SOURCE']['DB_PWD']
 
-src_engine = create_engine('postgresql://'+src_db_usr+':'+src_db_pwd+'@'+src_db_endpoint+':'+src_db_port+'/'+src_db_name+'')
-trg_engine = create_engine('postgresql://'+trg_db_usr+':'+trg_db_pwd+'@'+trg_db_endpoint+':'+trg_db_port+'/'+trg_db_name+'')
+    trg_db_endpoint = config['TARGET']['DB_ENDPOINT']
+    trg_db_port     = config['TARGET']['DB_PORT']
+    trg_db_name     = config['TARGET']['DB_NAME']
+    trg_db_usr      = config['TARGET']['DB_USR']
+    trg_db_pwd      = config['TARGET']['DB_PWD']
 
-# print('Read Metadata_Country_API_6_DS2_es_csv_v2.')
-# dfCountry = pd.read_csv('C:\\Users\\USER\OneDrive - Care Analytics, a division of TEAM International\\Proyects\\TallerAWS\\Data\\Metadata_Country_API_6_DS2_es_csv_v2.csv')
-# print('Loading Metadata_Country_API_6_DS2_es_csv_v2.')
-# dfCountry.to_sql('Country', src_engine,index=False,if_exists='append',schema = 'public')
+    table_name      = config['DATASET']['DATASET_NAME']
 
-# print('Read HDI_HumanDevelopmentIndex.')
-# dfHDI = pd.read_csv('C:\\Users\\USER\OneDrive - Care Analytics, a division of TEAM International\\Proyects\\TallerAWS\\Data\\HDI_HumanDevelopmentIndex.csv',encoding='utf-8')
-# print('Loading HDI_HumanDevelopmentIndex.')
-# dfHDI.to_sql('HumanDevelopmentIndex', src_engine,index=False,if_exists='append',schema = 'public')
+    #Create connectionstrings SOURCE/TARGET
+    src_engine = create_engine('postgresql+psycopg2://'+src_db_usr+':'+src_db_pwd+'@'+src_db_endpoint+':'+src_db_port+'/'+src_db_name+'',use_batch_mode=True)
+    trg_engine = create_engine('postgresql+psycopg2://'+trg_db_usr+':'+trg_db_pwd+'@'+trg_db_endpoint+':'+trg_db_port+'/'+trg_db_name+'',use_batch_mode=True)
 
-print('Read src table.')
-src_Data = pd.read_sql("""select * from "Country" order by 1 asc""",src_engine)
-print('Loading trg table.')
-src_Data.to_sql('Country', trg_engine,index=False,if_exists='append',schema = "public")
-print('trg table loaded.')
+    step_msg = "Reading source dataset." #Read source
+    print('Reading source dataset....') 
+    src_Data = pd.read_sql('select * from "'+table_name+'" order by 1 asc', src_engine)
 
-
-# insert_values = df.to_dict(orient='Country')
-# insert_statement = sqlalchemy.dialects.postgresql.insert(table).values(insert_values)
-# upsert_statement = insert_statement.on_conflict_do_update(
-#     constraint='fact_case_pkey',
-#     set_= df.to_dict(orient='dict')
-# )
+    step_msg = "Loading ["+table_name+"] table on target" #Loading target
+    print('Loading '+table_name+' table on target....') 
+    src_Data.to_sql(table_name, trg_engine,index=False,if_exists='replace',schema = "public", chunksize=1000)
+    print('Table '+table_name+' loaded on target!')
+    
+except Exception as e:
+        print("Error on process: "+step_msg +"\n Message: "+str(e))
